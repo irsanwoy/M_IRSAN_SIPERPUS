a861760567ab1218b4dc1f34089e31bf8addc3d4
irsanwoy <irsanwoy54@gmail.com>
2024-11-07 15:46:06 -0800

    buat permission role dan permission

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission as PermissionModels;
 
-class Permission extends Model
+
+class Permission extends PermissionModels
 {
     //
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as RoleModels;
 
-class Role extends Model
+ 
+
+class Role extends RoleModels
 {
     //
 }
