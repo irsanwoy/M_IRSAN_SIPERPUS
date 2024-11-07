9b09580818ec4cfc072549b6b08d9d1ed24177e7
Labtif Unsur <labtif.unsur@gmail.com>
2024-11-07 17:15:44 +0700

    Pertemuan 5 PWL-1

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Pustakawan A',
+            'email' => 'pustakawan@gmail.com',
+            'password' => Hash::make('pustakawan')
+        ]);
+        $user->assignRole('pustakawan');
+        $user->givePermissionTo('kelola_buku');
+        
+        $user = User::create([
+            'name' => 'Mahasiswa B',
+            'email' => 'mahasiswa@gmail.com',
+            'password' => Hash::make('mahasiswa')
+        ]);
+        $user->assignRole('member');
+        $user->givePermissionTo('lihat_buku');
+    }
+}
